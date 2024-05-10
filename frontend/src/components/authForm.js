361edabb9caf3b63ee310ae8338e443d3456f6e2
Oponjous <joe.ikdura@gmail.com>
2024-05-10 13:31:31 +0100

    commiting switching form between login and sign up

--- frontend/src/components/authForm.js
+++ frontend/src/components/authForm.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import classes from "./authForm.module.css";
 
 function AuthForm() {
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
   return (
     <form method="post" className={classes.form}>
-      <h1 className={classes.title}>Login or Create user Form</h1>
+      <h1 className={classes.title}>{isLogin ? "Login" : "Create new user"}</h1>
       <p className={classes.input}>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" name="email" />
@@ -14,8 +16,11 @@ function AuthForm() {
         <input id="password" type="Password" name="password" min={6} />
       </p>
       <p className={classes.auth_box}>
-        <Link to={"/auth"} className={classes.login}>
-          Login
+        <Link
+          to={`?mode=${isLogin ? "signup" : "login"}`}
+          className={classes.login}
+        >
+          {!isLogin ? "Login" : "Create new user"}
         </Link>
         <button className={classes.save}>Save</button>
       </p>
